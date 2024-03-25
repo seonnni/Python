2b4dad6d63116a83ed0626a36f9e0252f816e9f3
seonnni <162436180+seonnni@users.noreply.github.com>
2024-03-25 17:01:46 +0900

    [Silver IV] Title: 스택, Time: 76 ms, Memory: 34044 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,28 @@
+# 10828
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+N = int(input())
+dq = deque()
+
+for _ in range(N):
+    command = list(input().split())
+    if command[0] == 'push':
+        dq.append(command[1])
+    elif command[0] == 'pop':
+        if len(dq) > 0 :
+            tmp = dq.pop()
+            print(tmp)
+        else : print(-1)
+    elif command[0] == 'size':
+        print(len(dq))
+    elif command[0] == 'empty':
+        if len(dq) > 0 :
+            print(0)
+        else:
+            print(1)
+    elif command[0] == 'top':
+        if len(dq) > 0:
+            print(dq[-1])    
+        else: print(-1)
